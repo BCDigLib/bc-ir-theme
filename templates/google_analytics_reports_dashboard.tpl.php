2d9fe6263757f5aa4c47e478445707771f44c1fd
Emily Toner <emily.toner@bc.edu>
2014-09-29 15:14:44 -0400

    Custom GA Reports block template
    Remove graph and the link to report.

---
+++ templates/google_analytics_reports_dashboard.tpl.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * BC custom theme implementation to display the Google Analytics dashboard.
+ */
+?>
+
+<div class="google-analytics-summary google-analytics-reports">
+
+  <div class="google-analytics-pages">
+    <?php print $pages; ?>
+  </div>
+
+</div>
