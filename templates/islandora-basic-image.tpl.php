5d56e6aab216e98ca27cb7c6be076b06015a82f1
Emily Toner <emily.toner@bc.edu>
2014-12-08 16:12:22 -0500

    Remove "In Collections" from audio and image

--- templates/islandora-basic-image.tpl.php
+++ templates/islandora-basic-image.tpl.php
@@ -18,16 +18,6 @@
   </div>
   <div class="islandora-basic-image-metadata">
     <?php print $description; ?>
-    <?php if ($parent_collections): ?>
-      <div>
-        <h2><?php print t('In collections'); ?></h2>
-        <ul>
-          <?php foreach ($parent_collections as $collection): ?>
-            <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
-          <?php endforeach; ?>
-        </ul>
-      </div>
-    <?php endif; ?>
     <?php print $metadata; ?>
   </div>
 </div>
